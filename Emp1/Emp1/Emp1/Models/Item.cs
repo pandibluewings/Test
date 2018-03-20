50eb7c3a42dfd0d2b5509c599438254cf92112c0
Bala Chandran <balachandran.sivakumar@dciconsulting.co>
2018-03-20 14:59:24 +0530

    classes added
    class added in model

--- Emp1/Emp1/Emp1/Models/Item.cs
+++ Emp1/Emp1/Emp1/Models/Item.cs
@@ -13,4 +13,16 @@ namespace Emp1.Models
         string name { get; set; }
         string posi { get; set; }
     }
+ public class ItemCategory
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+ public class ItemMenu
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
 }

9731b5272ea02b0db3bf2b7f0d58e0a5f4338094
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-20 15:44:43 +0530

    removed classes

--- Emp1/Emp1/Emp1/Models/Item.cs
+++ Emp1/Emp1/Emp1/Models/Item.cs
@@ -13,15 +13,5 @@ namespace Emp1.Models
         string name { get; set; }
         string posi { get; set; }
     }
-    public class Item2
-    {
-        public string Id { get; set; }
-        public string Text { get; set; }
-        public string Description { get; set; }
-    }
-    public class EmpRec2
-    {
-        string name { get; set; }
-        string posi { get; set; }
-    }
+    
 }
